ee25bb180693a8b218f2b58bfa108b31b2c0e36f
william <z5311787@ad.unsw.edu.au>
2022-10-15 16:03:19 +1100

    removed api key

--- backend/src/server/config.js
+++ backend/src/server/config.js
@@ -1,6 +1,7 @@
 export const PORT = 5001;
 
-export const apiKey = '75be90f5aa3e4c72b3bbc2ee6a551e45';
+// PLEASE PROVIDE YOUR OWN API KEY HERE: 
+export const apiKey = '';
 
 export const ps5 = '187';
 export const ps4 = '18';
